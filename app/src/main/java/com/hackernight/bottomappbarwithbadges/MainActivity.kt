7106158c88cd0172cfb333a74075fc163dd53c1c
amitkumar <amitaero7@gmail.com>
2021-08-24 23:33:39 +0530

    As per theme color updated

--- app/src/main/java/com/hackernight/bottomappbarwithbadges/MainActivity.kt
+++ app/src/main/java/com/hackernight/bottomappbarwithbadges/MainActivity.kt
@@ -2,10 +2,18 @@ package com.hackernight.bottomappbarwithbadges
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.size
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView.background = null
+
     }
+
 }
