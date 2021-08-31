b9e6c5ac7f59fe2de54c7a8cde3261472ccd5c24
amitkumar <amitaero7@gmail.com>
2021-08-31 15:10:17 +0530

    Updated with ShapableImageView

--- app/src/main/java/com/hackernight/bottomappbarwithbadges/MainActivity.kt
+++ app/src/main/java/com/hackernight/bottomappbarwithbadges/MainActivity.kt
@@ -1,11 +1,10 @@
 package com.hackernight.bottomappbarwithbadges
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.counter_badge.*
+import kotlinx.android.synthetic.main.smart_action_btn.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +17,7 @@ class MainActivity : AppCompatActivity() {
         Glide
             .with(this)
             .load(R.drawable.smartanimation)
-            .override(40,40)
-            .into(fab)
+            .into(sImageView)
 
     }
 
